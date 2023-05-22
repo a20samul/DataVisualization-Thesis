dc26ed30909c986ea79651bddb1d0a39057ecd65
a20samul <81613484+a20samul@users.noreply.github.com>
2023-05-22 15:47:20 +0200

    D3: Code used to visualize US-counties and its data

--- D3/index.js
+++ D3/index.js
@@ -1,78 +1,73 @@
-svgHeight = 850;
-svgWidth = 1400;
+  svgHeight = 870;
+  svgWidth = 1810;
 
+  // Loads the data 
+ d3.queue()
+  .defer(d3.json, "us-county-boundaries.geojson")
+  .defer(d3.csv, "countydata.csv" ,function (d) { map.set(d.name, +d.population); })
+  .await(drawMap);
 
-// Loads the data asynchronously
-d3.queue()
-// .defer(d3.json, "countiesSWE.geojson")
-.defer(d3.json, "worldmap.geojson")
-.defer(d3.csv, "population-and-demography.csv" ,function (d) {
-  map.set(d.name, +d.population);
-})
-.await(drawMap);
+  // Creates the SVG with its attributes
+  const svg = d3.select("body")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth)
 
+  // Adds a g (container for grouping elements) to the SVG   
+  let g = svg.append("g")
+  
+  // Creates a SVG path from the data and maps the geographic projection
+  var projection = d3.geoMiller() // to visulize map in a certain projection
+    .scale(690)
+    .center([-120, 48])
+    .translate([svgWidth / 2, svgHeight / 2]);
+  // Converts data into SVG paths  
+  var geoPath = d3.geoPath(projection);
 
-// Creates the SVG with its attributes
-const svg = d3.select("body")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth)
+  // Color scheme of the map
+  var color = d3.scaleLinear()
+  .domain([1000, 5000, 10000, 15000, 30000, 60000, 120000, 240000, 60000])
+  .range(d3.schemeGreens[9]);
 
-// Adds a g (container for grouping elements) to the SVG   
-let g = svg.append("g")
+  // Creates a placeholder for a map
+  var map = d3.map();
 
-// Creates a SVG path from the data and maps the geographic projection
-var projection = d3.geoMiller() // to visulize map in a certain projection
-  .scale(200)
-  .center([16, 0])
-  .translate([svgWidth / 2, svgHeight / 2]);
-// Converts data into SVG paths  
-var geoPath = d3.geoPath(projection);
+  // Creates a tooltip
+  const tooltip = d3.select("body")
+    .append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
 
-// Color scheme of the map
-var color = d3.scaleLinear()
-.domain([10000, 1000000, 3000000, 10000000, 20000000, 80000000, 100000000, 300000000, 2000000000])
-.range(d3.schemeGreens[9]);
-
-// Creates a placeholder for a map
-var map = d3.map();
-
-// Creates a tooltip
-const tooltip = d3.select("body")
-  .append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-
-// Function which draws the map after the data has been loaded
-function drawMap(error, data) {
-  g.selectAll("path")
-    .data(data.features)
-    .enter() 
-    .append("path")
-    // Draws the paths
-      .attr("d", geoPath)
-      // Assigns colour from the color scheme
-      .attr("fill", function (d) {
-        {    
-          d.population = map.get(d.properties.name);
-          return color(d.population);        }
-      }) // Adds hovering effect and tooltip on mouseover
-      .on("mouseover", function(d)  {
-        d3.select(this).classed("hoverArea", true);
-        tooltip
-          .style("opacity", 1)
-          .style("top", (d3.event.pageY + 14) + "px")
-          .style("left", (d3.event.pageX + 22) + "px")
-          .transition()
-          .duration(300)
-          .text("Population " + d.properties.name + ': ' + d.population);
-    }
-    ) // Removes hovering effect and tooltip on mouseleave
-      .on("mouseleave", function(d)  {
-        d3.select(this).classed("hoverArea", false);
-        tooltip
-          .transition()
-          .duration(500)
-          .style("opacity", 0);
-      });
-}
+  // Function which draws the map after the data has been loaded
+  function drawMap(error, data) {
+    g.selectAll("path")
+      .data(data.features)
+      .enter() 
+      .append("path")
+      // Draws the paths
+        .attr("d", geoPath)
+        // Assigns colour from the color scheme
+        .attr("fill", function (d) {
+          {         
+            d.population = map.get(d.properties.name);
+            return color(d.population);        }
+        }) // Adds hovering effect and tooltip on mouseover
+        .on("mouseover", function(d)  {
+          d3.select(this).classed("hoverArea", true);
+          tooltip
+            .style("opacity", 1)
+            .style("top", (d3.event.pageY + 14) + "px")
+            .style("left", (d3.event.pageX + 22) + "px")
+            .transition()
+            .duration(300)
+            .text("County: " + d.properties.name + ', Population: ' + d.population);
+        }
+      ) // Removes hovering effect and tooltip on mouseleave
+        .on("mouseleave", function(d)  {
+          d3.select(this).classed("hoverArea", false);
+          tooltip
+            .transition()
+            .duration(500)
+            .style("opacity", 0);
+        });
+  }
